647612adfa52487f6d258fe547a73bbc1c7d68d1
Allan Jeremy <aj.dev254@gmail.com>
2021-08-11 17:02:05 +0300

    [Enhancement] Updated the todo entity to a typeorm entity

--- src/todo/entities/todo.entity.ts
+++ src/todo/entities/todo.entity.ts
@@ -1,6 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Todo {
-  id: string;
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
   task: string;
+
+  @Column()
   isDone: boolean;
+
+  @Column()
   description?: string | null;
 }
