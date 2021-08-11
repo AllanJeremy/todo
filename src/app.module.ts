3a1b5838aebb1a314f0d9c5d8b4e0bf26f3b6420
Allan Jeremy <aj.dev254@gmail.com>
2021-08-11 17:18:39 +0300

    Added typeorm config and connection in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Todo } from './todo/entities/todo.entity';
 import { TodoController } from './todo/todo.controller';
 import { TodoService } from './todo/todo.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'test_todo',
+      entities: [Todo],
+    }),
+  ],
   controllers: [AppController, TodoController],
   providers: [AppService, TodoService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private connection: Connection) {}
+}
